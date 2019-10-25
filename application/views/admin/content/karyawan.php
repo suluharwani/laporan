5afd569d858df52cbab4688cf394e7c3c664a3a5
Suluh Arwani <suluharwani007@gmail.com>
2019-10-25 10:05:45 +0700

    tunjangan

--- application/views/admin/content/karyawan.php
+++ application/views/admin/content/karyawan.php
@@ -46,6 +46,79 @@
 
 </div>
 <!-- End of Main Content -->
+<!-- Modal Tunjangan-->
+<div class="modal fade" id="modal_tunjangan_karyawan" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Tunjangan <span id="nama_karyawan"></span></h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form>
+          <div class="form-group row">
+            <label for="inputEmail3" class="col-sm-2 col-form-label">Nama Tunjangan</label>
+            <div class="col-sm-10">
+              <input type="text" class="form-control" id="id_karyawan" hidden>
+              <input type="text" class="form-control" id="nama_tunjangan" required>
+            </div>
+          </div>
+          <div class="form-group row">
+            <label for="inputPassword3" class="col-sm-2 col-form-label">Jumlah Tunjangan</label>
+            <div class="col-sm-10">
+              <input type="number" class="form-control" id="jumlah_tunjangan" required>
+            </div>
+          </div>
+          <div class="form-group row">
+            <div class="col text-center">
+              <button type="button" class="btn btn-primary tambah_tunjangan">Tambah</button>
+            </div>
+          </div>
+        </form>
+        <fieldset class="form-group">
+          <div class="card-body">
+            <div class="table-responsive">
+              <table class="table table-bordered" width="100%" cellspacing="0">
+                <thead>
+                  <tr>
+                    <th>No</th>
+                    <th>Nama Tunjangan</th>
+                    <th>Nominal</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+                <tfoot>
+                  <tr>
+                    <th>No</th>
+                    <th>Nama Tunjangan</th>
+                    <th>Nominal</th>
+                    <th>Action</th>
+                  </tr>
+                </tfoot>
+                <tbody id="data_tunjangan">
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </fieldset>
+        <div class="form-group row">
+          <label for="jatah_libur" class="col-sm-6 col-form-label">Total Tunjangan Bulanan</label>
+          <div class="col-sm-6" form-row>
+            <input type="number" class="form-control" id="total_tunjangan_bulanan" disabled>
+          </div>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</div>
+<!-- end modal Tunjangan -->
 <!-- Modal -->
 <div class="modal fade" id="modal_gaji_karyawan" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered" role="document">
@@ -75,16 +148,10 @@
               <legend class="col-form-label col-sm-2 pt-0">Shift 1</legend>
               <div class="col-sm-10 form-row">
                 <div class="form-group col-md-6">
-                  <select id="shiftmasuk1" class="form-control shiftmasuk1" >
-                    <option selected disabled>Masuk</option>
-                  </select>
+                  <input type="time" class="form-control" id="shiftmasuk1">
                 </div>
                 <div class="form-group col-md-6">
-                  <select id="shift1" class="form-control " >
-                    <option selected disabled>Pulang</option>
-
-                    <option>...</option>
-                  </select>
+                  <input type="time" class="form-control" id="shiftpulang1">
                 </div>
               </div>
             </fieldset>
@@ -93,17 +160,10 @@
                 <legend class="col-form-label col-sm-2 pt-0">Shift 2</legend>
                 <div class="col-sm-10 form-row">
                   <div class="form-group col-md-6">
-                    <select id="shift1" class="form-control " >
-                      <option selected disabled>Masuk</option>
-                      <option>...</option>
-                    </select>
+                    <input type="time" class="form-control" id="shiftmasuk2">
                   </div>
                   <div class="form-group col-md-6">
-                    <select class="form-control" id="pulang">
-                      <option selected disabled>Pulang</option>
-                      <option  value="1">1</option>
-                      <option  value="3">3</option>
-                    </select>
+                    <input type="time" class="form-control" id="shiftpulang2">
                   </div>
                 </div>
               </fieldset>
@@ -162,7 +222,8 @@
             '<td>'+data[i].status+'</td>'+
             // '<td>'+ status_contact +'</td>'+
             '<td style="text-align:center;">'+
-            '<a href="javascript:void(0);" class="btn btn-warning btn-sm karyawan_gaji" pulang2 = "'+data[i].pulang2+'" pulang1 = "'+data[i].pulang1+'" masuk2 = "'+data[i].masuk2+'" masuk1 = "'+data[i].masuk1+'" id_shift = "'+data[i].shift_id+'" karyawan_id="'+data[i].id+'" karyawan_nama="'+data[i].nama+'">Gaji</a>'+
+            '<a href="javascript:void(0);" class="btn btn-primary btn-sm karyawan_tunjangan" karyawan_id="'+data[i].id+'" karyawan_nama="'+data[i].nama+'">Tunjangan</a>'+
+            '<a href="javascript:void(0);" class="btn btn-warning btn-sm karyawan_gaji" pulang2 = "'+data[i].pulang2+'" pulang1 = "'+data[i].pulang1+'" masuk2 = "'+data[i].masuk2+'" masuk1 = "'+data[i].masuk1+'" id_shift = "'+data[i].shift_id+'" karyawan_id="'+data[i].id+'" karyawan_nama="'+data[i].nama+'">Gaji&Shift</a>'+
             '<a href="javascript:void(0);" class="btn btn-info btn-sm karyawan_informasi" karyawan_id="'+data[i].id+'" karyawan_nama="'+data[i].nama+'">Informasi</a>'+
             '<a href="javascript:void(0);" class="btn btn-danger btn-sm karyawan_delete" karyawan_id="'+data[i].id+'" karyawan_nama="'+data[i].nama+'">Hapus</a>'+
             '<a href="javascript:void(0);" class="btn btn-success btn-sm karyawan_aktivasi" karyawan_id="'+data[i].id+'" karyawan_nama="'+data[i].nama+'">Aktifkan</a>'+
@@ -182,30 +243,38 @@
       var masuk2 = $(this).attr('masuk2');
       var pulang1 = $(this).attr('pulang1');
       var pulang2 = $(this).attr('pulang2');
+      $('#id_karyawan').val(id);
+      $('#nama_karyawan').html(nama);
+      $('#shiftpulang1').val(pulang1);
+      $('#shiftpulang2').val(pulang2);
+      $('#shiftmasuk1').val(masuk1);
+      $('#shiftmasuk2').val(masuk2);
+      $('#modal_gaji_karyawan').modal('show');
+    });
+    $('#karyawan_list').on('click','.karyawan_tunjangan',function(){
+      var id = $(this).attr('karyawan_id');
+      var nama = $(this).attr('karyawan_nama');
+      $('#id_karyawan').val(id);
+      $('#nama_karyawan').html(nama);
+      $('#modal_tunjangan_karyawan').modal('show');
+    });
+    $('#tambah_tunjangan').on('click',function(){
+      var id = $('#id_karyawan').val();
+      var nama = $('#nama_tunjangan').val();
+      var jumlah = $('#jumlah_tunjangan').val();
       $.ajax({
-        url : "<?php echo site_url('admin/get_karyawan_shift');?>",
-        method : "POST",
-        data : {id: id},
-        async : true,
-        dataType : 'json',
+        type : "POST",
+        url  : "<?php echo site_url('admin/tambah_tunjangan')?>",
+        dataType : "JSON",
+        data : {id:id, nama:nama,jumlah:jumlah},
         success: function(data){
-          var html_masuk = '';
-          var i;
-          for(i=0; i<data.length; i++){
-            html_masuk += '<option value='+data[i].id+'>'+data[i].shift_masuk1+'</option>';
-          }
-          $('#shiftmasuk1').html(html_masuk);
-          $('#id_karyawan').val(id);
-          $('#nama_karyawan').html(nama);
-          $('#shiftpulang1').val(pulang1);
-          $('#shiftpulang2').val(pulang2);
-          $('.shiftmasuk1').val(masuk1);
-          $('#shiftmasuk2').val(masuk2);
-          $('#modal_gaji_karyawan').modal('show');
+          show_tunjangan();
+          swal ( "Sukses" ,  "Tunjangan Berhasil Ditambahkan!" ,  "success", {
+          buttons: false,
+          timer: 1000,
+        } );
         }
       });
-
-
-    // $("#sub_kategori_edit").val(sub_kategori);
-  });
-</script>
+      return false;
+    });
+    </script>
